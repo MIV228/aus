40bf32cbb5c43d2a6ebb78a7f0c07101f2444033
MIV228 <mivgithub@gmail.com>
2021-03-18 18:44:44 +0700

    dfgdfgfg

--- app/src/main/java/com/example/amogus/MainMenu.kt
+++ app/src/main/java/com/example/amogus/MainMenu.kt
@@ -1,11 +1,23 @@
 package com.example.amogus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val continue_game = findViewById<Button>(R.id.b_continue)
+        continue_game.setOnClickListener {
+            val ActivitySwitcher = Intent(this , Continue::class.java)
+            startActivity(ActivitySwitcher)
+        }
+
+
+
+
+
     }
 }
